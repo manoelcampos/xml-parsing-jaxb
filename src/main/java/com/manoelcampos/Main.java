40afa302c5cf9d3e8dbd66ccf5a221b192bbbae9
Manoel Campos <manoelcampos@gmail.com>
2021-09-22 21:31:30 -0300

    Refatora projeto e inclui conversão de objeto Java para XML. - Atualiza docs. - Cria Main.java.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ src/main/java/com/manoelcampos/Main.java
@@ -0,0 +1,45 @@
+package com.manoelcampos;
+
+import com.manoelcampos.products.Product;
+import com.manoelcampos.products.Products;
+import com.manoelcampos.xmlparsing.XmlParser;
+
+import java.io.IOException;
+
+/**
+ * Classe principal que executa o exemplo.
+ *
+ * @author Manoel Campos da Silva Filho
+ */
+public class Main {
+    /**
+     * URL de onde o arquivo XML será obtido.
+     */
+    private static final String XML_URL = "https://raw.githubusercontent.com/manoelcampos/xml-parsing-jaxb/master/products.xml";
+
+    /**
+     * Método de exemplo que mostra como usar a classe {@link XmlParser} para
+     * ler um arquivo XML contendo uma lista de produtos
+     * e então criar um objeto {@link Products} contendo os produtos
+     * lidos do arquivo.
+     * O exemplo também mostra como converter um objeto Java como {@link Products}
+     * de volta para XML.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            final XmlParser<Products> parser = new XmlParser();
+            System.out.println("# Convertendo XML contendo vários objetos Product para um objeto Products");
+            final Products products = parser.unmarshal(Products.class, XML_URL);
+            for (Product product : products.getProducts()) {
+                System.out.printf("\t%s%n", product);
+            }
+
+            System.out.println("\n# Convertendo o objeto Products (contendo uma lista de Product) de volta para XML");
+            System.out.println(parser.marshal(products));;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/com/manoelcampos/xmlparsing/XmlParser.java
+++ src/main/java/com/manoelcampos/xmlparsing/XmlParser.java
@@ -1,13 +1,10 @@
 package com.manoelcampos.xmlparsing;
 
-
-import com.manoelcampos.products.Product;
-import com.manoelcampos.products.Products;
-
+import javax.xml.bind.JAXB;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.URL;
 import java.net.URLConnection;
-import javax.xml.bind.JAXB;
 
 /**
  * Classe genêrica que lê um arquivo XML a partir de uma URL e transforma seu conteúdo 
@@ -18,68 +15,36 @@ import javax.xml.bind.JAXB;
  * @param <T> classe que representa a estrutura do arquivo XML (veja {@link #classe}).
  */
 public class XmlParser<T> {
-
     /**
-     * {@link URL} de onde será obtido o XML.
-     */
-    private final URL url;
-
-    /**
-     * Classe que representa o tipo de dados contido no XML. Após ser feito o parse do
-     * XML, um objeto de tal classe será criado para armazenar os dados lidos.
-     * Esta classe representa a estrutura do XML.
-     */
-    private final Class<T> classe;
-
-    /**
-     * Método de exemplo que mostra como usar a classe para
-     * ler um arquivo XML contendo uma lista de produtos
-     * e então criar um objeto {@link Products} contendo os produtos
-     * lidos do arquivo.
-     * 
-     * @param args 
-     */
-    public static void main(String[] args) {
-        try {
-            final XmlParser<Products> parser
-                    = new XmlParser(
-                            "https://raw.githubusercontent.com/manoelcampos/xml-parsing-jaxb/master/products.xml",
-                            Products.class);
-            final Products products = parser.parse();
-            for (Product product : products.getProducts()) {
-                System.out.println(product);
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    /**
-     * Instancia um XmlParser para ler um arquivo XML a partir de uma URL.
-     *
-     * @param urlStr URL do arquivo XML.
-     * @param classe classe que representa o tipo de dados contido no XML. Veja {@link #classe} para mais detalhes.
-     * @throws IOException quando o XML não puder ser lido (por exemplo, falha de conexão).
-     */
-    public XmlParser(final String urlStr, final Class<T> classe) throws IOException {
-        this.url = new URL(urlStr);
-        this.classe = classe;
-    }
-
-    /**
-     * Lê o arquivo XML a partir da {@link #url} e faz o parse (unmarshall),
+     * Lê o arquivo XML a partir do url e faz o parse (unmarshall),
      * convertendo tal XML para um objeto Java contendo os dados lidos do arquivo.
-     * Veja {@link #classe} para mais detalhes.
      *
-     * @return um objeto da classe indicaca por {@link #classe}, contendo
+     * @param targetClass classe que representa o tipo de dados contido no XML.
+     *               Após ser feito o parse do XML, um objeto de tal classe será criado para armazenar os dados lidos.
+     *               Esta classe representa a estrutura do XML.
+     * @param urlStr URL do arquivo XML.
+     * @return um objeto da classe indicaca no parâmetro "classe", contendo
      *         os dados lidos do arquivo XML
      * @throws IOException quando o XML não puder ser lido (por exemplo, falha de conexão).
      */
-    private T parse() throws IOException {
+    public T unmarshal(final Class<T> targetClass, final String urlStr) throws IOException {
+        final URL url = new URL(urlStr);
         final URLConnection conn = url.openConnection();
         conn.connect();
 
         //A stream é automaticamente fechada pelo método unmarshal
-        return JAXB.unmarshal(conn.getInputStream(), classe);
+        return JAXB.unmarshal(conn.getInputStream(), targetClass);
+    }
+
+    /**
+     * Converte um objeto do tipo T passado e gera uma String da
+     * representação XML dele.
+     * @param object objeto a ser convertido para XML
+     * @return String do XML convertido
+     */
+    public String marshal(final T object) {
+        final StringWriter writer = new StringWriter();
+        JAXB.marshal(object, writer);
+        return writer.toString();
     }
 }
